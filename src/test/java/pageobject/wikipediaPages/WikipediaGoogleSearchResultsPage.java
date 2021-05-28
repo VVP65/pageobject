ab626e7cdad2814f1698cf01bf8302756d4a674e
Volodymyr Protsiv <Volodymyr_Protsiv@epam.com>
2021-05-28 22:58:37 +0300

    return statement moved

--- src/test/java/pageobject/wikipediaPages/WikipediaGoogleSearchResultsPage.java
+++ src/test/java/pageobject/wikipediaPages/WikipediaGoogleSearchResultsPage.java
@@ -37,6 +37,7 @@ public class WikipediaGoogleSearchResultsPage extends AbstractPage {
 
     public WikipediaGoogleSearchResultsPage openFirstSearchResultLink() {
         firstSearchResultLink.submit();
+
         return this;
     }
 }
