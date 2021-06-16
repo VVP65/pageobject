a2f63a2479c1ffcd0015ebc45df76b53c831282c
Volodymyr Protsiv <Volodymyr_Protsiv@epam.com>
2021-06-16 14:13:57 +0300

    Methods moved to JQuerySortablePage as redundant

--- src/test/java/pageobject/basePage/BasePage.java
+++ src/test/java/pageobject/basePage/BasePage.java
@@ -4,24 +4,13 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.FindAll;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.List;
-
 public abstract class BasePage {
     protected WebDriver driver;
     protected static final int TIME_OUT_SECONDS = 20;
-
-    @FindAll({@FindBy(xpath = "//li[@class='ui-state-default ui-sortable-handle']")})
-    public List<WebElement> sortableList;
-
-    protected static final int X_OFF_SET = 0;
-    protected static final int Y_OFF_SET = 283;
-    protected static final int Y_OFF_DELTA = 44;
 
     protected BasePage(WebDriver driver) {
         this.driver = driver;
@@ -48,18 +37,6 @@ public abstract class BasePage {
     protected void highlightElement(WebElement element) {
         JavascriptExecutor javascript = ((JavascriptExecutor) driver);
         javascript.executeScript("arguments[0].style.border='3px solid green'", element);
-    }
-
-    protected void clickJqueryAboutSectionElementWithJavaScriptExecutor() {
-        JavascriptExecutor jsExec = ((JavascriptExecutor) driver);
-        jsExec.executeScript("document.getElementById('alex-dovenmuehle').click()");
-    }
-
-    protected void dragAndDropElementByCoordinates() {
-        for (int i = 0; i < sortableList.size() - 1; i++) {
-            new Actions(driver).dragAndDropBy(sortableList.get(0), X_OFF_SET, Y_OFF_SET - i * Y_OFF_DELTA).build().perform();
-            waiter(driver);
-        }
     }
 
     protected void clickJqueryAboutSection(WebElement element) {
--- src/test/java/pageobject/jQuerySortablePage/JquerySortablePage.java
+++ src/test/java/pageobject/jQuerySortablePage/JquerySortablePage.java
@@ -1,12 +1,20 @@
 package pageobject.jQuerySortablePage;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import pageobject.basePage.BasePage;
 
+import java.util.List;
+
 public class JquerySortablePage extends BasePage {
     private static final String JQUERY_SORTABLE_PAGE_URL = "https://jqueryui.com/sortable/#placeholder";
+    protected static final int X_OFF_SET = 0;
+    protected static final int Y_OFF_SET = 283;
+    protected static final int Y_OFF_DELTA = 44;
 
     @FindBy(css = "#content > iframe")
     private WebElement sortableListFrame;
@@ -16,6 +24,9 @@ public class JquerySortablePage extends BasePage {
 
     @FindBy(css = "#menu-top > li:nth-child(8) > a")
     private WebElement jQueryAboutSection;
+
+    @FindAll({@FindBy(xpath = "//li[@class='ui-state-default ui-sortable-handle']")})
+    public List<WebElement> sortableList;
 
     public JquerySortablePage(WebDriver driver) {
         super(driver);
@@ -29,6 +40,17 @@ public class JquerySortablePage extends BasePage {
         openPage(JQUERY_SORTABLE_PAGE_URL);
 
         return this;
+    }
+    protected void clickJqueryAboutSectionElementWithJavaScriptExecutor() {
+        JavascriptExecutor jsExec = ((JavascriptExecutor) driver);
+        jsExec.executeScript("document.getElementById('alex-dovenmuehle').click()");
+    }
+
+    protected void dragAndDropElementByCoordinates() {
+        for (int i = 0; i < sortableList.size() - 1; i++) {
+            new Actions(driver).dragAndDropBy(sortableList.get(0), X_OFF_SET, Y_OFF_SET - i * Y_OFF_DELTA).build().perform();
+            waiter(driver);
+        }
     }
 
     public String firstSortableElementText() {
