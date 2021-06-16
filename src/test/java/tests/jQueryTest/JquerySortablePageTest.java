4811de34909016ae0c0f828e74bb1dbe56dd58f3
Volodymyr Protsiv <Volodymyr_Protsiv@epam.com>
2021-06-16 14:34:23 +0300

    var name changed

--- src/test/java/tests/jQueryTest/JquerySortablePageTest.java
+++ src/test/java/tests/jQueryTest/JquerySortablePageTest.java
@@ -8,11 +8,11 @@ import tests.BaseTest;
 public class JquerySortablePageTest extends BaseTest {
     @Test
     public void jquerySortablePage() {
-        String jquerySortablePage = new JquerySortablePage(driver)
+        String firstSortableElementText = new JquerySortablePage(driver)
                 .openJquerySortablePage()
                 .getFirstSortableElementText();
 
-        logger.info(String.format("Sortable list contains %s", jquerySortablePage));
-        Assert.assertTrue(jquerySortablePage.contains("Item 7"), "This element does not refer to first element in a sorted list");
+        logger.info(String.format("Sortable list contains %s", firstSortableElementText));
+        Assert.assertTrue(firstSortableElementText.contains("Item 7"), "This element does not refer to first element in a sorted list");
     }
 }
