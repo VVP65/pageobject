9a489e1ffc0a98711a6cc532dbe7b8a1af823754
Volodymyr Protsiv <Volodymyr_Protsiv@epam.com>
2021-06-23 22:34:23 +0300

    Update TestDataReader.java

--- src/test/java/service/TestDataReader.java
+++ src/test/java/service/TestDataReader.java
@@ -6,7 +6,6 @@ public class TestDataReader {
     private static ResourceBundle resourceBundle = ResourceBundle.getBundle(System.getProperty("environment"));
 
     public static String getTestData(String key) {
-
         return resourceBundle.getString(key);
     }
 }
