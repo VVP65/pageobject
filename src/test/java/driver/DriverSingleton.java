3ac1d9ff5ebcc61fce4b682947d12f95cda3876a
Volodymyr Protsiv <Volodymyr_Protsiv@epam.com>
2021-06-29 13:33:25 +0300

    Singleton fixed

--- src/test/java/driver/DriverSingleton.java
+++ src/test/java/driver/DriverSingleton.java
@@ -12,17 +12,18 @@ public class DriverSingleton {
     }
 
     public static WebDriver getDriver() {
-        if (null == driver) {
+        if (driver == null) {
             switch (System.getProperty("browser")) {
                 case "firefox": {
                     WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
+                    break;
                 }
                 case "chrome": {
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
+                    break;
                 }
-
                 default: {
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
