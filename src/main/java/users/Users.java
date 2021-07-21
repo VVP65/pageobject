6232bac35cd8765bbd838912759838cbc7dd09eb
Volodymyr Protsiv <Volodymyr_Protsiv@epam.com>
2021-07-21 23:36:46 +0300

    To POM file dependencies added, To "Rest Assured" Test bunch of tests added

---
+++ src/main/java/users/Users.java
@@ -0,0 +1,9 @@
+package users;
+
+public class Users {
+    private Integer userId;
+
+    public Integer getUserId() {
+        return userId;
+    }
+}
---
+++ src/test/java/tests/webserviceTest/RestAssuredTest.java
@@ -0,0 +1,44 @@
+package tests.webserviceTest;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import users.Users;
+
+public class RestAssuredTest {
+    @BeforeTest
+    public void initialTest() {
+        RestAssured.baseURI = "http://jsonplaceholder.typicode.com";
+    }
+
+    @Test
+    public void checkStatusCode() {
+        Response response = RestAssured.when()
+                .get("/users")
+                .andReturn();
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
+
+    @Test
+    public void checkResponseHeader() {
+        Response response = RestAssured.when()
+                .get("/users")
+                .andReturn();
+
+        String responseContentTypeHeader = response.getHeader("Content-Type");
+        Assert.assertEquals(responseContentTypeHeader, "application/json; charset=utf-8");
+    }
+
+    @Test
+    public void checkResponseBody() {
+        Response response = RestAssured.when()
+                .get("/users")
+                .andReturn();
+        ResponseBody<?> responseBody = response.getBody();
+        Users[] posts = responseBody.as(Users[].class);
+        Assert.assertEquals(posts.length, 10);
+    }
+}
